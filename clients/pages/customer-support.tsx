72cb5aaea88cded580abf7e908f888cdf06d32d2
TuanKietTran <kyletran101.work@gmail.com>
2024-07-23 02:03:43 +0700

    add profile pages

---
+++ clients/pages/customer-support.tsx
@@ -0,0 +1,6 @@
+import ProfileSection from "@/components/ProfileSection";
+import { navigation } from "./me";
+
+export default function CustomerSupportPage() {
+  return <ProfileSection urls={navigation}></ProfileSection>;
+}
---
+++ clients/pages/me/payment-info.tsx
@@ -0,0 +1,6 @@
+import ProfileSection from "@/components/ProfileSection";
+import { navigation } from ".";
+
+export default function PaymentInfoPage() {
+  return <ProfileSection urls={navigation}></ProfileSection>;
+}
