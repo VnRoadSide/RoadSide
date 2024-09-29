8a53a73eeba1f61a1a5a9d11444add5473fda364
TuanKietTran <kyletran101.work@gmail.com>
2024-09-30 00:37:57 +0700

    update error

--- clients/app/global-error.tsx
+++ clients/app/global-error.tsx
@@ -1,22 +1,44 @@
-'use client'
-import { Button } from "@mantine/core"
+"use client";
+import { Footer } from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import theme from "@/theme";
+import { AppShell, AppShellHeader, AppShellMain, Button, ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { NavigationProgress } from "@mantine/nprogress";
 
- // Error boundaries must be Client Components
- 
+// Error boundaries must be Client Components
+
 export default function GlobalError({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   return (
     // global-error must include html and body tags
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <Button onClick={() => reset()}>Try again</Button>
+    <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <ColorSchemeScript />
+      </head>
+      <body suppressHydrationWarning={true}>
+        <MantineProvider theme={theme}>
+          <NavigationProgress />
+
+          <AppShell
+            withBorder={false}
+          >
+            <AppShellMain>
+              <h2>Something went wrong!</h2>
+              <Button onClick={() => reset()}>Try again</Button>
+            </AppShellMain>
+          </AppShell>
+          <Footer />
+        </MantineProvider>
       </body>
     </html>
-  )
+  );
 }
